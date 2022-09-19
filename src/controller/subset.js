cb93c08c23d87ac79c27b9127e758e277d52ba87
“lijinke” <lijinke@xiaoyun.com>
2022-09-19 18:59:06 +0800

    add

--- src/controller/subset.js
+++ src/controller/subset.js
@@ -10,8 +10,8 @@ router.get('/', async function (ctx) {
     let datas = []
     if(!lib) return ctx.body = {datas}
     if(['疑难词','简单词'].includes(lib)){
-        let ids = await Subset.find({userid:ctx.session.userid,type:['疑难词','简单词'].indexOf(lib)},'id')
-        ids = ids.map(v=>v.id)
+        let ids = await Subset.find({userid:ctx.session.userid,type:['疑难词','简单词'].indexOf(lib)},'vid')
+        ids = ids.map(v=>v.vid)
         if(ids.length>0) datas = await Vocabulary.find({id:ids},'id,en,zh,phonetic')
     }else datas = await Vocabulary.find({classify:lib},'id,en,zh,phonetic') 
 
@@ -32,7 +32,7 @@ router.get('/del', async function (ctx) {
     const {id,lib} = ctx.query
     let type = ['疑难词','简单词'].indexOf(lib)
     if(![0,1].includes(type) || !id) return ctx.body={status:1,msg:'数据不合法'}
-    let result = await Subset.del({userid:ctx.session.userid,id,type})
+    let result = await Subset.del({userid:ctx.session.userid,vid:id,type})
     if(result) ctx.body={status:0}
     else ctx.body={status:1,msg:'操作失败，请刷新后再试'}
 })
