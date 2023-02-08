370f4dac9a58f5b951a1a1c0f30109e22850c1b5
“lijinke” <lijinke@xiaoyun.com>
2023-02-08 18:25:59 +0800

    整合分析代码

--- src/infrastructureAnalysis.js
+++ src/infrastructureAnalysis.js
@@ -4,36 +4,36 @@ function r(p){l(Reflect.ownKeys(p))}
 function main(app){
   
   let mls = app.middleware
+  let rs = []
 
   mls.map(v=>{
     let name = v.name
-    if(name === 'dispatch') routerParse(v.router)
+    if(name === 'dispatch') rs.push(routerParse(v.router))
     else l('中间件',name)
   })
+
+  l(rs.flat())
 }
 
-// 路由解析 返回 results
+// 路由解析 返回 [{url:'请求路径',method:'请求方法',params:'请求参数',body:'代码'}]
 function routerParse(router){
 
-  let Caches = [{url:'请求路径',method:'请求方法',params:'请求参数',body:'代码'}]
+  // {url:'请求路径',method:'请求方法',params:'请求参数',body:'代码'}
+  let Caches = []
   layerParse(router,Caches)
-
   // l(Caches)
   return Caches
-  
 }
 
 function layerParse(layer,caches,flag){
   const {opts:{end},methods,stack,path,regexp} = layer
-  l(flag)
   if(!end){
     stack.map(v=>{
-      if(v.otps) layerParse(v,caches,'inner')
+      if(v.opts) layerParse(v,caches,'inner')  
     })
   }else{
-    caches.push({url:path,method:methods.join(','),params:'',code:stack,pattern:regexp})
+    caches.push({url:path,method:methods.join(','),params:'',code:stack&&stack.join(),pattern:regexp})
   }
-  // l(caches)
 }
 
 
